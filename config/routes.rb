558a5f4934bf31f1bf3d6126e64bb698d362f7c5
Angela Amat y Leon <angela.amatyleon@kellogg.northwestern.edu>
2024-03-08 19:01:57 +0000

    WIP for user auth

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,11 @@ Rails.application.routes.draw do
   resources "places"
   resources "sessions"
   resources "users"
+
+  get "/login", :controller => "sessions", :action => "new"
+  get "/logout", :controller => "sessions", :action => "destroy"
+
+  # Custom route for landing page (aka root route)
+  # get "/", :controller => "posts", :action => "index"
+
 end
