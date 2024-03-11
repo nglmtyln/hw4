3ed60e7a697627681003d5ef3b16df6c194cf9b2
Angela Amat y Leon <angela.amatyleon@kellogg.northwestern.edu>
2024-03-11 00:51:37 +0000

    images got working

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,12 +4,21 @@ class EntriesController < ApplicationController
   end
 
   def create
+
+    if @current_user != nil
+
     @entry = Entry.new
     @entry["title"] = params["title"]
     @entry["description"] = params["description"]
     @entry["occurred_on"] = params["occurred_on"]
     @entry["place_id"] = params["place_id"]
+    @entry.uploaded_image.attach(params["uploaded_image"])
     @entry.save
+
+  else
+    flash["notice"] = "Login first."
+  end
+
     redirect_to "/places/#{@entry["place_id"]}"
   end
 
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,6 @@
 class Entry < ApplicationRecord
+
+  has_one_attached :uploaded_image
+
+
 end
